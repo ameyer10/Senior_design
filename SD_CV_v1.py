ebbf3c2b4c694cc0ac08beaa6fcf69083eeb03bc
ameyer10 <100802099+ameyer10@users.noreply.github.com>
2022-12-06 11:32:06 -0600

    Update SD_CV_v1.py

--- SD_CV_v1.py
+++ SD_CV_v1.py
@@ -2,23 +2,21 @@
 # 10/30/21
 # Main_pb
 from typing import Union, Any
-from matplotlib import pyplot as plt
-
 
 import cv2
 import numpy as np
 
-capture = cv2.VideoCapture(0)  # select camera that the program will use
+capture = cv2.VideoCapture(1)  # select camera that the program will use
 
 waitTothrow = 0
 
 while True:
     isTrue, frame = capture.read()
-    lower_ir = np.array([220, 220, 220])  # setting the high and low BGR (not RGB) values
+    lower_ir = np.array([200, 200, 200])  # setting the high and low BGR (not RGB) values
     upper_ir = np.array([255, 255, 255])  # brightest pixels have highest values from 0-255
 
     mask = cv2.inRange(frame, lower_ir, upper_ir)  # creates mask
-    kernel = np.ones((5, 5), np.uint8)  # creates 3x3 square kernel for morphology
+    kernel = np.ones((3, 3), np.uint8)  # creates 3x3 square kernel for morphology
     mask = cv2.erode(mask, kernel, iterations=1)  # erodes mask with 3x3 square
     mask = cv2.dilate(mask, kernel, iterations=1)  # dilates mask with 3x3 square
 
@@ -35,24 +33,11 @@ while True:
         avgX = 1
         avgY = 1
 
-
-# attempt using histogram
-    hist = cv2.calcHist([mask], [0], None, [256], [0, 256])
-    plt.hist(hist.ravel(), 256, [0, 256]);
-    plt.show()
-
-
-# attempt using contours and open CV library functions
-#     contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#     cv2.drawContours(frame, contours, 0, (0,255,0),3)
-
-
-    # this is the attempt at itterating through the matrix below
-    # print(avgY)
-
+    print(avgY)
+    #
     # upperArray = 0
     # lowerArray = 0
-
+    #
     # for i in range(len(mask)):
     #     for j in range(len(mask[0])):
     #         if mask[i][j] != 0:
@@ -69,6 +54,9 @@ while True:
     # print(upperArray)
     # print("lower array value")
     # print(lowerArray)
+
+    cv2.imshow("mask", mask)  # displays mask for testing
+
 
 
     # cv2.circle(mask, (avgX, avgY), 8, (255,0,0), -1)  # make dot and put it on the average of all the positive pixels
